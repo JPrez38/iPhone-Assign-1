116ff10c0e27c6414f7b22798248e6b8c96a57e3
Jacob Preston <jacobpreston4@gmail.com>
2013-02-17 20:15:10 -0500

    Final build. It works!!!

--- Assignment1/main.m
+++ Assignment1/main.m
@@ -13,17 +13,23 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        SiteValue *china = [SiteValue siteValueWithUsername:@"Hello" andPassword:@"ddfsd"];
-        SiteValue *hello = [SiteValue siteValueWithUsername:@"JPREZ" andPassword:@"helloworld"];
-        SiteValue *gmail = [SiteValue siteValueWithUsername:@"jacobo" andPassword:@"preston"];
-        SiteValue *twitter = [SiteValue siteValueWithUsername:@"jac" andPassword:@"pdsfsd"];
+        SiteValue *gmail = [SiteValue siteValueWithUsername:@"Hello" andPassword:@"Jacob"];
+        SiteValue *nyu = [SiteValue siteValueWithUsername:@"JPREZ" andPassword:@"helloworld"];
+        SiteValue *facebook = [SiteValue siteValueWithUsername:@"jacobo" andPassword:@"preston"];
+        SiteValue *twitter = [SiteValue siteValueWithUsername:@"jprez" andPassword:@"password"];
+        SiteValue *amazon = [SiteValue siteValueWithUsername:@"jac" andPassword:@"ob"];
+        SiteValue *yahoo = [SiteValue siteValueWithUsername:@"packerfan" andPassword:@"bro"];
+        SiteValue *tumblr = [SiteValue siteValueWithUsername:@"tumble" andPassword:@"around"];
         
         NSMutableDictionary *login = [NSMutableDictionary dictionary];
         
-        [login setObject:china forKey:@"China"];
-        [login setObject:hello forKey:@"facebook"];
         [login setObject:gmail forKey:@"gmail"];
+        [login setObject:nyu forKey:@"nyu"];
+        [login setObject:facebook forKey:@"facebook"];
         [login setObject:twitter forKey:@"twitter"];
+        [login setObject:amazon forKey:@"amazon"];
+        [login setObject:yahoo forKey:@"yahoo"];
+        [login setObject:tumblr forKey:@"tumblr"];
         
         
         for (NSString *key in login) {
@@ -31,11 +37,24 @@ int main(int argc, const char * argv[])
             [[login objectForKey:key] printSiteValue];
         }
         
+        printf("\n");
+        
         [login removeObjectForKey:@"twitter"];
+        [login removeObjectForKey:@"facebook"];
+        [login removeObjectForKey:@"tumblr"];
         
         for (NSString *key in login) {
             [[login objectForKey:key] incrementCount];
         }
+        
+        printf("\n");
+        
+        int i;
+        for (i=0;i<3;i++){
+            [[login objectForKey:@"amazon"] incrementCount];
+        }
+        [[login objectForKey:@"gmail"] incrementCount];
+        [[login objectForKey:@"nyu"] incrementCount];
         
         for (NSString *key in login) {
             NSLog(@"For key %@:", key);
